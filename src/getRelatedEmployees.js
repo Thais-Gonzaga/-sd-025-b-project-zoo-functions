baaf3f4af0346fcf4b9d31b813b1f54b3fcf9aaf
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-08-26 12:50:16 -0300

    feat: Implementando a função getRelatedEmployees.
     #ref: requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,16 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function isManager(id) {
-  // seu código aqui
-}
+const isManager = (id) => (employees.some((e) => (e.managers).find((j) => (j === id))));
 
-function getRelatedEmployees(managerId) {
-  // seu código aqui
-}
+const teste = (managerId) => (employees
+  .filter((e) => (e.managers).find((j) => (j === managerId))))
+  .map((el) => `${el.firstName} ${el.lastName}`);
+
+const getRelatedEmployees = (managerId) => {
+  console.log(teste(managerId));
+  if (teste(managerId).length !== 0) return teste(managerId);
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+};
 
 module.exports = { isManager, getRelatedEmployees };
