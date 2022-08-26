5dc6420679a02c7accd6346b9c53144fec1c6605
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-08-26 16:15:22 -0300

    feat:  Implementando a função countAnimals.
    ref# requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+const { species } = data;
+
+const teste = (animal) => {
+  const { specie, sex } = animal;
+  const qtd = Object.keys(animal).length;
+  if (qtd === 1) return (species.find((e) => (e.name === specie))).residents.length;
+  return (species.find((e) => (e.name === specie))).residents.filter((j) => j.sex === sex).length;
+};
+
+const teste1 = () => species.reduce((acc, curr) => {
+  acc[curr.name] = curr.residents.length;
+  return acc;
+}, {});
+
+const countAnimals = (animal) => ((!animal) ? teste1() : teste(animal));
+
+console.log(countAnimals({ specie: 'penguins' }));
 
 module.exports = countAnimals;
