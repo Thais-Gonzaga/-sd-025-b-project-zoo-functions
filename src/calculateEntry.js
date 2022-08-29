0d25ccf7e52c41641d6dff69fecf89b5c83b72af
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-08-29 18:23:45 -0300

    feat:  Implementando a função calculateEntry.
     ref# requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,20 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui
-}
+const childs = (entrants) => entrants.filter((ages) => ages.age < 18).map((e) => e.age).length;
+const adults = (entrants) => (
+  entrants.filter((ages) => ages.age >= 18 && ages.age < 50).map((e) => e.age).length);
+const seniors = (entrants) => entrants.filter((ages) => ages.age >= 50).map((e) => e.age).length;
+const countEntrants = (entrants) => ({
+  adult: adults(entrants),
+  child: childs(entrants),
+  senior: seniors(entrants),
+});
 
-function calculateEntry(entrants) {
-
-}
+const calculateEntry = (entrants) => {
+  if (!Array.isArray(entrants) || (!entrants)) return 0;
+  return (([countEntrants(entrants)].map((obj) => obj.adult) * 49.99)
+  + ([countEntrants(entrants)].map((obj) => obj.child) * 20.99)
+  + ([countEntrants(entrants)].map((obj) => obj.senior) * 24.99));
+};
 
 module.exports = { calculateEntry, countEntrants };
