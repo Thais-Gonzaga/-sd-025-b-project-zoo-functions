3c867f4853b95971e4ef11e7af7a1bddab5648ea
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-08-30 20:20:50 -0300

    feat: Implementando a função getOldestFromFirstSpecies.
     ref# requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+const { employees, species } = data;
+
+const getOldestFromFirstSpecies = (ids) => {
+  const [arrayResponsi] = employees.find(({ id }) => id === ids).responsibleFor;
+  const { residents } = species.find(({ id }) => id === arrayResponsi);
+  const [old] = residents.sort((a, b) => b.age - a.age);
+  return Object.values(old);
+};
 
 module.exports = getOldestFromFirstSpecies;
